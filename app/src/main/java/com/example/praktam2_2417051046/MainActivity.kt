dde733510736d8d168402590c0a4fe3ccc0723fc
MFarisAdithya <m.farisadithya@gnail.com>
2026-03-19 20:16:19 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam2_2417051046/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051046/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : ComponentActivity() {
                 ) { innerPadding ->
                     HomeWorkoutApp(
                         modifier = Modifier.padding(innerPadding)
+
                     )
                 }
             }
